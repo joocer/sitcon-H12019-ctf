d353b9a1c8b695ce7fc8b0fc3fc894e274556beb
Justin Joyce <justin@joocer.local>
2019-09-10 08:26:54 +0100

    fixed typo

--- branleys_two/avalanche.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from app.models import User, Post
-from waitress import serve
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
-
-serve(app, host='0.0.0.0', port=80)
